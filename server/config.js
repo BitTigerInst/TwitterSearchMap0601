1769a1e1986ffbdb36414998145583487a910d99
chengchen886 <charlie@ccs.neu.edu>
2016-10-13 16:38:41 +0800

    test config
    test config

---
+++ server/config.js
@@ -0,0 +1,15 @@
+const config = {
+	mongoURL:
+	port:
+
+	consumer_key: 'Yw0wlIUKSs43sxwh3hVO06h0j'
+	consumer_secret: 'ONxI8bW4QTjxQiRd8rVfmn9TFtds8KA2F0fQ77aZGJfa8VY0Ey'
+	access_token: '751779502150160384-Bcf8tBW7Vo6wYY7cfRs0DXv9toACzgA'
+	access_token_secret: 'AXy0Dsj9D2IiGCg02Z44PfQdstxlYrh9rITd33D2yq4dH'
+
+	indexName:
+	host:
+	log:
+};
+
+export default config;
